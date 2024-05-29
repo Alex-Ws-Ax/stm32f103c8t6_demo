d71e162bbd3fea3681dfe078ea56786201560453
ws <ws@ws.com>
2024-05-29 20:33:28 +0800

    add uart2 debug

--- common/debug.c
+++ common/debug.c
@@ -3,12 +3,24 @@
 #include "stm32f10x_usart.h"
 
 #define     DEBUG_CMD_PARSE_EN  0
-#define     DEBUG_UART      USART1
+#define     DEBUG_UART      (DEBUG_UART2)
 #define     MAX_RX_LEN      64
 
 unsigned char  rx_buf[MAX_RX_LEN] = {0};
 unsigned char  rx_index = 0;
 unsigned short debug_level = DEBUG_ALL;
+
+#if (DEBUG_UART == DEBUG_UART1)
+#define DEBUG_HW_UART   USART1
+#elif (DEBUG_UART == DEBUG_UART2)
+#define DEBUG_HW_UART   USART2
+#endif
+
+#if (DEBUG_UART == DEBUG_UART1)
+#define    debug_handler    USART1_IRQHandler
+#elif (DEBUG_UART == DEBUG_UART2)
+#define    debug_handler    USART2_IRQHandler
+#endif
 
 void UART_GPIO_Config(void)
 {
@@ -16,6 +28,7 @@ void UART_GPIO_Config(void)
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
+#if (DEBUG_UART == DEBUG_UART1)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -24,6 +37,16 @@ void UART_GPIO_Config(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
     GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_10;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
+#elif (DEBUG_UART == DEBUG_UART2)
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_3;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+#endif
 }
 
 
@@ -32,34 +55,42 @@ void DebugInit(void)
     USART_InitTypeDef uart_init_t = {0};
     NVIC_InitTypeDef nvic_init_t = {0};
 
-    UART_GPIO_Config();
+
+#if (DEBUG_UART == DEBUG_UART1)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+#elif (DEBUG_UART == DEBUG_UART2)
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+#endif
+    
+    
+    UART_GPIO_Config();
     USART_StructInit(&uart_init_t);
-    USART_Init(DEBUG_UART, &uart_init_t);
+    USART_Init(DEBUG_HW_UART, &uart_init_t);
+#if DEBUG_CMD_PARSE_EN
+    USART_ITConfig(DEBUG_UART, USART_IT_RXNE, ENABLE);
 
-//    USART_ITConfig(DEBUG_UART, USART_IT_RXNE, ENABLE);
+    nvic_init_t.NVIC_IRQChannel = USART1_IRQn;
+    nvic_init_t.NVIC_IRQChannelPreemptionPriority  = 0;
+    nvic_init_t.NVIC_IRQChannelSubPriority         = 0;
+    nvic_init_t.NVIC_IRQChannelCmd                 = ENABLE;
 
-//    nvic_init_t.NVIC_IRQChannel = USART1_IRQn;
-//    nvic_init_t.NVIC_IRQChannelPreemptionPriority  = 0;
-//    nvic_init_t.NVIC_IRQChannelSubPriority         = 0;
-//    nvic_init_t.NVIC_IRQChannelCmd                 = ENABLE;
-
-//    NVIC_Init(&nvic_init_t);
-    USART_Cmd(DEBUG_UART, ENABLE);
+    NVIC_Init(&nvic_init_t);
+#endif
+    USART_Cmd(DEBUG_HW_UART, ENABLE);
 }
-
-void USART1_IRQHandler(void)
+#if DEBUG_CMD_PARSE_EN
+void debug_handler(void)
 {
-    if (USART_GetITStatus(DEBUG_UART, USART_IT_RXNE) == SET)
+    if (USART_GetITStatus(DEBUG_HW_UART, USART_IT_RXNE) == SET)
     {
-        unsigned char data = USART_ReceiveData(DEBUG_UART);
+        unsigned char data = USART_ReceiveData(DEBUG_HW_UART);
         if (rx_index < MAX_RX_LEN)
         {
             if ((data == 0x0A) || (data == 0x0D))
             {
-#if DEBUG_CMD_PARSE_EN
+
                 cmd_parsing((char *)rx_buf);
-#endif
+
                 rx_index = 0;
                 memset(rx_buf, 0, MAX_RX_LEN);
             }
@@ -73,16 +104,16 @@ void USART1_IRQHandler(void)
             rx_index = 0;
             memset(rx_buf, 0, MAX_RX_LEN);
         }
-        USART_ClearITPendingBit(DEBUG_UART, USART_IT_RXNE);
+        USART_ClearITPendingBit(DEBUG_HW_UART, USART_IT_RXNE);
     }
 
 }
-
+#endif
 int fputc(int ch, FILE *p)
 {
-    USART_SendData(DEBUG_UART, (u8)ch);
+    USART_SendData(DEBUG_HW_UART, (u8)ch);
 
-    while (USART_GetFlagStatus(DEBUG_UART, USART_FLAG_TXE) == RESET);
+    while (USART_GetFlagStatus(DEBUG_HW_UART, USART_FLAG_TXE) == RESET);
 
     return ch;
 }
--- common/debug.h
+++ common/debug.h
@@ -2,6 +2,11 @@
 #define __DEBUG_H_
 #include <stdio.h>
 
+#define     DEBUG_UART1         1
+#define     DEBUG_UART2         2
+#define     DEBUG_UART3         3
+#define     DEBUG_UART4         4
+#define     DEBUG_UART5         5
 
 #define DEBUG_EN       1
 
